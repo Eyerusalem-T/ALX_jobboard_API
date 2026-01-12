934229f5713988b4c5385cd8459663d7ca519d97
eyerusalem <4u.aplus.tutor@gmail.com>
2026-01-12 09:16:24 +0300

    Remove job_board_api.code-workspace and .gitignore files as part of project cleanup

--- config/config/settings.py
+++ config/config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-from pathlib import Path
 import dj_database_url
 from decouple import config
 import sys
@@ -109,7 +108,13 @@ if DATABASE_URL:
         conn_max_age=600,
         conn_health_checks=True,
     )
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
